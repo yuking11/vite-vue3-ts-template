ea8336e741d3c258ec43ffefc965806bdaaabadb
yuking11 <ys11.works@gmail.com>
2021-10-27 01:35:07 +0900

    style: 不要コード削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,6 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
